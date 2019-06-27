f9b5e5fba030d1648b68fd9e12c22a100fee4e49
Andrey Nechaev <nechaevfmg@mail.ru>
2019-06-27 16:16:37 +0500

    первый коммит

---
+++ script.js
@@ -0,0 +1,22 @@
+/*alert('hello');
+[1, 2, 3].push();*/
+
+
+let money = 16990; 
+console.log('money: ', money);
+
+let income = 6; 
+console.log('income: ', income);
+
+let addExpenses = 990;
+console.log('addExpenses: ', addExpenses);
+
+let deposit = 16990;
+console.log('deposit: ', deposit);
+
+let mission = 'FrotenedDeveloper';
+console.log('mission: ', mission);
+
+let period = 6;
+console.log('period: ', period);
+
