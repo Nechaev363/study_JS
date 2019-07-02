cbe9320854d6dad4cf79af35810c461ee0c18d0b
Nechaev363 <52284268+Nechaev363@users.noreply.github.com>
2019-07-02 22:58:00 +0500

    Add files via upload

---
+++ lesson_3/hard_hw/script.js
@@ -0,0 +1,35 @@
+'use script';
+
+/*let langRu = ['Понедельник ', 'Вторник ', 'Среда ', 'Четверг ', 'Пятница ', 'Суббота ', 'Воскресенье'];
+let ru = [langRu];
+let langEn = ['Monday ', 'Tuesday ', 'Wednes­day ', 'Thursday ', 'Friday ', 'Saturday ', 'Sunday'];
+let en = [langEn];
+let lang = '';*/
+
+/*if(lang === ru) {
+    console.log(ru);
+} else if(lang === en) {
+    console.log(en);
+}*/
+
+/*switch (lang) {
+    case ru: console.log(ru);
+    break;
+    case en : console.log(en);
+    break;
+}*/
+
+ /*let lang = new Array();
+ lang = [];
+ lang[0] =  ['Понедельник ', 'Вторник ', 'Среда ', 'Четверг ', 'Пятница ', 'Суббота ', 'Воскресенье'];
+ lang[1] =  ['Monday ', 'Tuesday ', 'Wednes­day ', 'Thursday ', 'Friday ', 'Saturday ', 'Sunday'];
+
+
+console.log(lang[0]);
+console.log(lang[1]);*/
+let namePerson = '';
+let acess = namePerson === 'Артем' ? console.log('Директор') : console.log('студент');
+let acess1 = namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
+ 
+
+
