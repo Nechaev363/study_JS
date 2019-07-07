210d19e99c6f7f0292e4647e4faf0f2172c89341
Nechaev363 <52284268+Nechaev363@users.noreply.github.com>
2019-07-07 13:59:24 +0500

    Add files via upload

---
+++ lesson_3/hard_hw/script.js
@@ -0,0 +1,23 @@
+'use script';
+
+
+
+let ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+
+let en = ['Monday', 'Tuesday', 'Wednes­day', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+let lang = [ru];
+lang = [en];
+if (lang[ru]) {
+ console.log(ru);
+} else if (lang[en]){
+    console.log(en);
+}
+
+
+
+// let namePerson = '';
+// let acess = namePerson === 'Артем' ? console.log('Директор') : console.log('студент');
+// let acess1 = namePerson === 'Максим' ? console.log('преподаватель') : console.log('студент');
+ 
+
+
