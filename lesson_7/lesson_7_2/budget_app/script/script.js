7a1225fc2ce9969ddf024eb4664ee34f8fdc59e5
Nechaev363 <52284268+Nechaev363@users.noreply.github.com>
2019-07-09 16:56:15 +0500

    Add files via upload

---
+++ lesson_7/lesson_7_2/budget_app/script/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let sum = document.getElementById('start');
+console.log(sum);
+
+let incomeAdd = document.getElementsByTagName('button');
+let expensesAdd = document.getElementsByTagName('button');
+
+console.log(incomeAdd, expensesAdd);
+
+let chekBox = document.querySelector('#deposit-check');
+
+console.log(chekBox);
+
+let input = document.querySelectorAll('.additional_income-item');
+console.log(input);
+
+let value = document.querySelectorAll('.result-total');
+console.log(value);
+
+let inputBefore1 = document.querySelector('input[type="text"]');
+console.log(inputBefore1);
+let inputBefore2 = document.querySelector('input[type="range"]');
+console.log(inputBefore2);
+
